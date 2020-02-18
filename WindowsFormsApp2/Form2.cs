a635026c41077f403b072c96724d6e2193fce98c
Frolov333 <56120173+Frolov333@users.noreply.github.com>
2020-02-18 18:02:56 +0400

    Add files via upload

--- WindowsFormsApp2/Form2.cs
+++ WindowsFormsApp2/Form2.cs
@@ -14,31 +14,51 @@ namespace WindowsFormsApp2
     {
         public int part;
         public string name;
+        public Button button2;
         public pers(int part1, string name1)
         {
             part = part1;
             name = name1;
+            button2 = new Button();
         }
     }
 
     public partial class Form2 : Form
     {
-        pers[] lyudi = new pers[100];
+        pers[] lydi = new pers[100];
         public Form2()
         {
             InitializeComponent();
-            lyudi[0] = new pers(2, "sidor");
+            lydi[0] = new pers(1, "Сидор");
+            lydi[1] = new pers(2, "Шустрый");
+            lydi[2] = new pers(3, "Меченый");
+
+            for (int i = 0; i < 3; i++)
+            {
+                lydi[i].button2.Location = new Point(340, 40);
+                lydi[i].button2.Size = new Size(290, 40);
+                lydi[i].button2.Text = lydi[i].name;
+                Controls.Add(lydi[i].button2);
+            }
+
+
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
 
         }
-    } // private void button1_Click(object sender, EventArgs e)
-  //  {
-  //        for(int i = 0; i < 4; i++)
-   //         {
-   //        if (pers[i].part < Convert.ToInt32(ComboBox1.Text))
-   //         }
-  //  }
-//}   
+        private void button1_Click(object sender, EventArgs e)
+        {
+              for(int i = 0; i < 3; i++)
+               {
+                if (lydi[i].part < Convert.ToInt32(button1.Text))
+                {
+
+                }
+              }
+        }
+                                                                                                                                      
+
+    }
+}  
